b2c0144a16a2c5cdae2b1fbc6859e9e048e7e6a9
Almeida154 <davidalmeida154of@gmail.com>
2021-11-17 16:10:00 -0300

    :sparkles: Voltando com a splash

---
+++ public/js/splash.js
@@ -0,0 +1,6 @@
+const overlay = document.querySelector(".overlay");
+window.addEventListener("load", function () {
+  overlay.style.opacity = "0";
+  overlay.style.transition = "0.6s";
+  overlay.style.display = "none";
+});
